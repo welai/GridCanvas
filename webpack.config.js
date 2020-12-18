d6a6a64d4ebbbda978c6ebaca59dde92e6c41507
celestialphineas <hanghang0713@hotmail.com>
2020-12-18 18:08:31 +0800

    Packing update

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,12 @@ function generateConfig(name) {
   var uglify = name.indexOf('min') > -1;
   var config = {
     mode:   'development',
+    devtool: 'source-map',
     entry:  './src/index.ts',
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: name + '.js',
-      sourceMapFilename: name + '.map',
+      sourceMapFilename: name + '.js.map',
       library: 'gridcanvas',
       libraryTarget: 'umd',
       umdNamedDefine: true
@@ -32,14 +33,17 @@ function generateConfig(name) {
       ]
     }
   };
-  if(uglify) {
+  if (uglify) {
     config.optimization = {
-      minimize: true, minimizer: [ new TerserPlugin() ]
+      minimize: true, minimizer: [ new TerserPlugin({
+        terserOptions: { format: { comments: false } },
+        extractComments: false
+      }) ]
     }
     config.mode = 'production';
+    config.devtool = undefined;
   }
   return config;
 }
 
 module.exports = ['gridcanvas', 'gridcanvas.min'].map((name) => generateConfig(name));
-
